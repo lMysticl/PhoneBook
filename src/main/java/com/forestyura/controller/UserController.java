4300e6e92f6e45ade50993b6cc3d0810dab2cff9
forestyura <forestyura@gmail.com>
2017-03-10 23:37:22 +0200

    fix UserController

--- src/main/java/com/forestyura/controller/UserController.java
+++ src/main/java/com/forestyura/controller/UserController.java
@@ -2,7 +2,6 @@ package com.forestyura.controller;
 
 import com.forestyura.model.entity.User;
 import com.forestyura.model.service.UserService;
-import com.forestyura.service.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;

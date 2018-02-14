0aac81975c4186c71ca210ece5725b8db727c1fa
lMysticl <putrenkov99@gmail.com>
2018-02-14 23:47:26 +0200

    -deploy

--- src/test/java/com/mystic/service/LoginServiceImplTest.java
+++ src/test/java/com/mystic/service/LoginServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.mystic.service;
 
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,5 +12,6 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Ignore
 public class LoginServiceImplTest {
 }
--- src/test/java/com/mystic/service/Test.java
+++ src/test/java/com/mystic/service/Test.java
@@ -5,6 +5,7 @@ package com.mystic.service;
  * @version 1.0
  * @since
  */
+
 public class Test {
 //    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 //    String hashedPassword = passwordEncoder.encode(obj.getString("password").toLowerCase());
